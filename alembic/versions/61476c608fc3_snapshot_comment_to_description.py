4468da136e08750654fa21554f09661722abfa4d
zdomke <zdomke@slac.stanford.edu>
2026-02-20 10:51:56 -0800

    ENH: Change snapshot column name 'comment' --> 'description'

---
+++ alembic/versions/61476c608fc3_snapshot_comment_to_description.py
@@ -0,0 +1,26 @@
+"""snapshot_comment_to_description
+
+Revision ID: 61476c608fc3
+Revises: 003_perf_indexes
+Create Date: 2026-02-20 10:30:10.488591
+
+"""
+from collections.abc import Sequence
+
+from alembic import op
+
+# revision identifiers, used by Alembic.
+revision: str = "61476c608fc3"
+down_revision: str | None = "003_perf_indexes"
+branch_labels: str | Sequence[str] | None = None
+depends_on: str | Sequence[str] | None = None
+
+
+def upgrade() -> None:
+    # Rename the 'comment' column to 'description' in the 'snapshot' table.
+    op.alter_column("snapshot", "comment", new_column_name="description")
+
+
+def downgrade() -> None:
+    # Rename the 'description' column back to 'comment' in the 'snapshot' table.
+    op.alter_column("snapshot", "description", new_column_name="comment")
